251e18e5486b1cb09c8666bb037f81f188b45fb7
Fredson Chaves <fredsonchaves07@gmail.com>
2021-03-07 11:58:13 -0300

    fix: error on tests

--- src/database/index.ts
+++ src/database/index.ts
@@ -14,6 +14,9 @@ export default async(): Promise <Connection> => {
                 entities: [
                     Letter
                 ],
+                migrations: [
+                    __dirname + "/migrations/**"
+                ]
             })
         )
     } catch (exception) {
