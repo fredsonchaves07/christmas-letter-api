2ea22b78fc6cebef97b9eb9586b500767f9775d8
Fredson Chaves <fredsonchaves07@gmail.com>
2021-03-07 13:40:02 -0300

    fix: registration error and data query and delay in server startup

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,22 @@ import express from 'express'
 import createConnection from './database'
 import { router } from './routes'
 
-createConnection()
-
 const app = express()
 
-app.use(express.json())
-app.use(router)
+createConnection().then(async () => {
+    app.use(express.json())
+    app.use(router)
+
+    app.listen(5000, () => {
+        console.log('Server is Runnning')
+    })
+})
 
 export { app }
+
+
+
+
+
+
+
